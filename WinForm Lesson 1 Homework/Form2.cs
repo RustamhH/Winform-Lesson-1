087db9405a2aea5df1ef02833cb5134b382c4e9d
Hesenli Rustam Ismayil <Hesen_zc39@itstep.edu.az>
2023-05-26 21:58:59 +0400

    last

--- WinForm Lesson 1 Homework/Form2.cs
+++ WinForm Lesson 1 Homework/Form2.cs
@@ -19,23 +19,24 @@ namespace WinForm_Lesson_1_Homework
             InitializeComponent();
         }
 
+        Timer timer1 = new Timer();
+        Timer timer2 = new Timer();
         private void Baku_Click(object sender, EventArgs e)
         {
+            timer2.Stop();
             BackgroundImage = Resources.Baku_Logo1;
-            Timer timer = new Timer();
-            timer.Interval = 1000;
-            timer.Tick += (oo, ee) => Text = DateTime.Now.ToLongTimeString();
-            timer.Start();
+            timer1.Interval = 1000;
+            timer1.Tick += (oo, ee) => Text = DateTime.Now.ToLongTimeString();
+            timer1.Start();
         }
 
         private void London_Click(object sender, EventArgs e)
         {
+            timer1.Stop();
             BackgroundImage = Resources.London_Logo1;
-            Timer timer = new Timer();
-            timer.Interval = 1000;
-            DateTime london = DateTime.Now.AddHours(-3);
-            timer.Tick += (oo, ee) => Text = london.ToLongTimeString();
-            timer.Start();
+            timer2.Interval = 1000;
+            timer2.Tick += (oo, ee) => Text = DateTime.Now.AddHours(-3).ToLongTimeString();
+            timer2.Start();
         }
 
         private void button1_Click(object sender, EventArgs e)
